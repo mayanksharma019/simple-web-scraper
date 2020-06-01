ffe7238303ca6a21808879c29f6edfcd7bf862c5
mayanksharma019 <45957631+mayanksharma019@users.noreply.github.com>
2020-06-01 19:04:06 +0530

    final commit

---
+++ webscraper.py
@@ -0,0 +1,17 @@
+import urllib.request
+from bs4 import BeautifulSoup
+class scraper:
+    def __init__(self,site):
+        self.site=site
+
+     def scrape(self):
+         r=urllib.request.urlopen(self.site)
+         html=r.read()
+         parser='html.parser'
+         sp=BeautifulSoup(html,parser)
+         fsor tag in sp.find_all('a'):
+             url=tag.get('href')
+             print("\n"+url)
+
+scrape=scraper('https://news.google.com')
+scrape.scrape()
